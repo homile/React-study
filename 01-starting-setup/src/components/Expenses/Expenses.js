ad0a132912d8e72c0d379ea8be952b0c57a78aae
homile <ssanho2@naver.com>
2022-07-08 01:02:12 +0900

    연도 필터링 구현

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -13,6 +13,20 @@ function Exoenses(props) {
     setFilteredYear(selectedYear);
   };
 
+  const filterExpenses = props.expenses.filter((el) => {
+    // getFullYear: new Date로 생성한 날짜 중 연도를 가져온다.
+    return el.date.getFullYear().toString() === filteredYear;
+  });
+
+  // 렌더링될 요소를 미리 정의하여 변수만 JSX로 작성해서 코드의 가독성을 높일 수 있다.
+  let expensesContent = <p>expenses 데이터가 없습니다!</p>
+
+  if(filterExpenses.length > 0){
+    expensesContent = filterExpenses.map((el) => {
+      return <ExpenseItem key={el.id} expenses={el} />
+    })
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -28,9 +42,16 @@ function Exoenses(props) {
           컴포넌트를 맵핑할 때는 key를 지정해주어여 한다.
           리액트가 개별의 컴포넌트로 인식하게 해주는 고유 식별 값이다.
           데이터에 고유 id가 없다면 index로 지정해주면 된다. */}
-        {props.expenses.map((el) => {
-          return <ExpenseItem key={el.id} expenses={el} />;
-        })}
+
+        {/* {filterExpenses.length === 0 ? (
+          // &&를 사용하여 표현식을 짧게 할 수 있다.
+          <p>expenses 데이터가 없습니다!</p>
+        ) : (
+          filterExpenses.map((el) => {
+            return <ExpenseItem key={el.id} expenses={el} />;
+          })
+        )} */}
+        {expensesContent}
       </Card>
     </div>
   );
