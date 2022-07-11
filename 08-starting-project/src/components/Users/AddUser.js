6f0013db8e4cc2396ebcc6ecedf48b8f3d536d67
homile <ssanho2@naver.com>
2022-07-12 01:28:26 +0900

    addUser 구현

--- 08-starting-project/src/components/Users/AddUser.js
+++ 08-starting-project/src/components/Users/AddUser.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
 function AddUser(props) {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (e) => {
     e.preventDefault();
+
+    // 유효성 검사(basic)
+    // 입력값이 없을 경우
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+      return;
+    }
+
+    // 나이가 1살보다 미만일 경우
+    if(+enteredAge < 1){
+      return;
+    }
+    
+    props.onAddUser(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
@@ -13,10 +40,20 @@ function AddUser(props) {
         {/* htmlFor: label에 for속성을 부여
       for속성: for값과 id의 값이 같으면 연결된다. */}
         <label htmlFor="username">Username</label>
-        <input id="username" type="text"></input>
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        ></input>
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number"></input>
-        <button type="submit">Add User</button>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        ></input>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
