92db214904c77cf7d5a2e08915cef8fff81c901f
homile <ssanho2@naver.com>
2022-07-12 01:45:48 +0900

    error 모달 구현

--- 08-starting-project/src/components/Users/AddUser.js
+++ 08-starting-project/src/components/Users/AddUser.js
@@ -2,25 +2,35 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
+import ErrorModal from "../UI/ErrorModal";
 
 function AddUser(props) {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = (e) => {
     e.preventDefault();
 
     // 유효성 검사(basic)
     // 입력값이 없을 경우
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: '입력값이 잘못되었습니다.',
+        message: '이름 및 나이를 입력해 주세요!'
+      });
       return;
     }
 
     // 나이가 1살보다 미만일 경우
-    if(+enteredAge < 1){
+    if (+enteredAge < 1) {
+      setError({
+        title: '입력값이 잘못되었습니다.',
+        message: '알맞은 나이를 입력해 주세요!'
+      });
       return;
     }
-    
+
     props.onAddUser(enteredUsername, enteredAge);
     setEnteredUsername("");
     setEnteredAge("");
@@ -34,28 +44,35 @@ function AddUser(props) {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  }
+
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        {/* htmlFor: label에 for속성을 부여
+    <>
+      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          {/* htmlFor: label에 for속성을 부여
       for속성: for값과 id의 값이 같으면 연결된다. */}
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={enteredUsername}
-          onChange={usernameChangeHandler}
-        ></input>
-        <label htmlFor="age">Age (Years)</label>
-        <input
-          id="age"
-          type="number"
-          value={enteredAge}
-          onChange={ageChangeHandler}
-        ></input>
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          ></input>
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          ></input>
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </>
   );
 }
 
