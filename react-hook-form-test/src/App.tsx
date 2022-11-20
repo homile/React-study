af02ae19ed75da293b3c2adefa3a8a7248386825
homile <ssanho2@naver.com>
2022-11-20 18:25:37 +0900

    feat:react-hook-form / yup을 이용한 유효성 검사 완료

--- react-hook-form-test/src/App.tsx
+++ react-hook-form-test/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import Signup from './pages/signup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Signup></Signup>
     </div>
   );
 }
--- react-hook-form-test/src/pages/signup.tsx
+++ react-hook-form-test/src/pages/signup.tsx
@@ -24,9 +24,9 @@ const schema = yup.object().shape({
   email: yup.string().email().required("이메일을 입력해주세요."),
   password: yup
     .string()
-    .min(8)
+    .min(8, "비밀번호는 8글자 이상입니다..")
     // 최대 글자수 지정
-    .max(16)
+    .max(16, "비밀번호는 16글자 이하입니다.")
     .matches(
       /^(?=.*[a-zA-Z])((?=.*\d)(?=.*\W))/,
       "비밀번호는 영문, 숫자, 특수문자가 포함되어야 합니다."
@@ -39,8 +39,53 @@ const schema = yup.object().shape({
     .oneOf([yup.ref("password"), null], "비밀번호가 일치하지 않습니다."),
 });
 
-const signup = () => {
-  return <div>signup</div>;
+const Signup = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({ resolver: yupResolver(schema), mode: "onChange" });
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  return (
+    // handleSubmit은 onSubmit을 호출하기 전에 입력의 유효성을 검사한다.
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="userId">아이디</label>
+      <input
+        id="userId"
+        type="text"
+        placeholder="아이디를 입력해주세요"
+        {...register("id")}
+      />
+      {errors.id && <span>{errors.id.message}</span>}
+      <label htmlFor="email">이메일</label>
+      <input
+        type="email"
+        id="email"
+        placeholder="example@example.com"
+        {...register("email")}
+      />
+      {errors.email && <span>{errors.email.message}</span>}
+      <label htmlFor="password">비밀번호</label>
+      <input
+        type="password"
+        id="password"
+        placeholder="비밀번호를 입력해주세요"
+        {...register("password")}
+      />
+      {errors.password && <span>{errors.password.message}</span>}
+      <label htmlFor="confrimPassword">비밀번호 확인</label>
+      <input
+        type="password"
+        id="confrimPassword"
+        placeholder="비밀번호를 입력해주세요"
+        {...register("confirmPassword")}
+      />
+      {errors.confirmPassword && <span>{errors.confirmPassword.message}</span>}
+      <button type="submit">회원가입</button>
+    </form>
+  );
 };
 
-export default signup;
+export default Signup;
